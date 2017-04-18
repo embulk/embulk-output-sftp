0ec254bed1f92585773d6131b2db70a76fea2ff5
sakama <satoshiakama@gmail.com>
2017-04-18 14:42:35 +0900

    Close all resources before uploading if current resource is opened

--- src/main/java/org/embulk/output/sftp/SftpFileOutput.java
+++ src/main/java/org/embulk/output/sftp/SftpFileOutput.java
@@ -177,9 +177,10 @@ public class SftpFileOutput
                         @Override
                         public Void call() throws IOException, RetryGiveupException
                         {
-                            if (!currentFile.getContent().isOpen()) {
-                                currentFileOutputStream = new BufferedOutputStream(currentFile.getContent().getOutputStream());
+                            if (currentFile.getContent().isOpen()) {
+                                currentFile.getContent().close();
                             }
+                            currentFileOutputStream = new BufferedOutputStream(currentFile.getContent().getOutputStream());
                             currentFileOutputStream.write(buffer.array(), buffer.offset(), buffer.limit());
                             return null;
                         }
