4ae6231f1ed64c321870da0361bdabb5e99300ca
Robert Nguyen <ng.hung83@gmail.com>
2018-08-24 14:15:15 +0700

    Remove obsolete `callNonInterruptible()` method

--- src/main/java/org/embulk/output/sftp/utils/TimedCallable.java
+++ src/main/java/org/embulk/output/sftp/utils/TimedCallable.java
@@ -1,8 +1,5 @@
 package org.embulk.output.sftp.utils;
 
-import org.embulk.spi.Exec;
-import org.slf4j.Logger;
-
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
@@ -14,18 +11,6 @@ import java.util.concurrent.TimeoutException;
 public abstract class TimedCallable<V> implements Callable<V>
 {
     private static final ExecutorService THREAD_POOL = Executors.newCachedThreadPool();
-    private Logger logger = Exec.getLogger(getClass());
-
-    public V callNonInterruptible(long timeout, TimeUnit timeUnit)
-    {
-        try {
-            return call(timeout, timeUnit);
-        }
-        catch (Exception e) {
-            logger.warn("Time-out call failed, ignore and resume", e);
-            return null;
-        }
-    }
 
     public V call(long timeout, TimeUnit timeUnit)
             throws InterruptedException, ExecutionException, TimeoutException
